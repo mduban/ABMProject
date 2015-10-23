513c60abcb3e5bd5c906f4208a944b111659a357
Iwo Mateusz Dubaniowski <iwomateuszdubaniowski@Iwos-MacBook-Pro.local>
2015-10-23 15:54:12 +0800

    Initial MATLAB file

---
+++ main.m
@@ -0,0 +1,13 @@
+%% Modelling hospital-acquired infections
+% Agent-based modelling course project
+% Singapore-ETH Centre (SEC)
+% Future Resilient Systems
+% Authors:
+%   *   Mateusz Iwo Dubaniowski
+%   *   Tianyin Sun
+
+display('Start simulation...');
+
+
+
+display('Simulation completed');
