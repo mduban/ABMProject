e5d2aef8228feff6ffe41761b5308e799acfc049
Iwo Mateusz Dubaniowski <iwomateuszdubaniowski@Iwos-MacBook-Pro.local>
2015-10-26 12:38:18 +0800

    Started simulation work

--- main.m
+++ main.m
@@ -12,13 +12,17 @@ display('Start simulation...');     %Started!
 
 %% Initilization
 TIMESTEPS = 10;
-PROPERTIES = 4;
+PROPERTIES = 4;         % Properties: 1 - patient(1)/staff(0), 2 - room no.
 NSTAFF = 10;
-NPATIENTS = 30;
+NPATIENTS = 46;
 WARDSMAP = [1 2 3 2 1; 1 1 2 3 3; 2 2 -1 2 3; 3 2 0 1 3; 2 2 1 3 1;]
 dimensions = size(WARDSMAP);
 wards = zeros(dimensions(1), dimensions(2));
 agents = zeros(NSTAFF+NPATIENTS, PROPERTIES);
+agents(1:1:NPATIENTS, 1)=1;
+for i=NPATIENTS+1:1:NPATIENTS+NSTAFF
+    agents(i, 2)=round((rand*(dimensions(1)*dimensions(2))-1)+0.5);
+end
 % Probabilities
 infectionPatient=0.1;
 infectionStaff=0.01;
@@ -34,6 +38,9 @@ for i=1:1:dimensions(1)*dimensions(2)
 end
 for i=1:1:dimensions(1)*dimensions(2)
     movementProbMat(i,:) = movementProbMat(i,:)/sum(movementProbMat(i,:));
+    for j=dimensions(1)*dimensions(2):-1:1
+       movementProbMat(i,j) = sum(movementProbMat(i,1:1:j));
+    end
 end
 
 
@@ -43,10 +50,12 @@ for tstep=1:1:TIMESTEPS
     tstep
 
 %% Iterate over agents
-
-
+    for agent=1:1:NSTAFF+NPATIENTS
+    
 %% Update agents
+        
 
+    end
 end
 
 %% Save data
